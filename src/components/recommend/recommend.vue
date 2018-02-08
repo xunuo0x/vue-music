551f85e341c6efd8736742a6c9e506e0fc29f7ad
xunuo0x <1175176908@qq.com>
2018-02-08 15:05:15 +0800

    chapter3

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    推荐页面
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+import fastclick from 'fastclick'
+
+import './common/stylus/index.styl'
+
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  router, // 此router为new出来的Router,在App.vue中即可使用
   render: h => h(App)
 })
