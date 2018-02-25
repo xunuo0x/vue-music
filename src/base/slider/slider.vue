8e2ffe7b452b2edf1050461684402c469140056b
xunuo0x <1175176908@qq.com>
2018-02-25 13:14:13 +0800

    test

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -54,7 +54,6 @@ export default {
   methods: {
     _setSliderWidth (isResize) {
       this.children = this.$refs.sliderGroup.children
-
       let width = 0
       let sliderWidth = this.$refs.slider.clientWidth
       for (let i = 0; i < this.children.length; i++) {
