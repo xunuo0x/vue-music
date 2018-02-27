190962ddd3623e8d8a7b4b8e0e9636201375a473
xunuo0x <1175176908@qq.com>
2018-02-27 22:02:33 +0800

    单个歌手页面

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,225 @@
+<template>
+  <scroll class="listview"
+   :data="data"
+   ref="listview"
+   :listenScroll="listenScroll"
+   :probeType="probeType"
+   @scroll="scroll"
+   >
+    <ul>
+      <li class="list-group" v-for="group in data" :key="group.title" ref="listGroup">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li @click="selectItem(item)" class="list-group-item" v-for="item in group.items" :key="item.id">
+            <img class="avatar" v-lazy="item.avatar"/>
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
+      <ul>
+        <li class="item" v-for="(item,index) in shortcutList"
+        :key="index"
+        :class="{'current': currentIndex === index}"
+        :data-index="index"
+        >
+          {{item}}
+        </li>
+      </ul>
+    </div>
+    <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <h1 class="fixed-title">{{fixedTitle}}</h1>
+    </div>
+    <div class="loading-container" v-if="!data.length">
+      <loading></loading>
+    </div>
+  </scroll>
+</template>
+
+<script>
+import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
+import { getData } from 'common/js/dom'
+
+const ANCHOR_HEIGHT = 18
+const TITLE_HEIGHT = 30
+
+export default {
+  data () {
+    return {
+      scrollY: -1,
+      currentIndex: 0,
+      diff: 0
+    }
+  },
+  created () {
+    this.touch = {}
+    this.listenScroll = true
+    this.probeType = 3
+    this.listHeight = []
+  },
+  components: {
+    Scroll, Loading
+  },
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    shortcutList () {
+      return this.data.map((group) => {
+        return group.title.substr(0, 1)
+      })
+    },
+    fixedTitle () {
+      if (this.scrollY > 0) {
+        return ''
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : null
+    }
+  },
+  methods: {
+    selectItem (item) {
+      this.$emit('select', item)
+    },
+    onShortcutTouchStart (e) {
+      let anchorIndex = parseInt(getData(e.target, 'index'))
+      let firstTouch = e.touches[0]
+      this.touch.y1 = firstTouch.pageY
+      this.touch.anchorIndex = anchorIndex
+      this._scrollTo(anchorIndex)
+    },
+    onShortcutTouchMove (e) {
+      let firstTouch = e.touches[0]
+      this.touch.y2 = firstTouch.pageY
+      let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
+      let anchorIndex = this.touch.anchorIndex + delta
+      this._scrollTo(anchorIndex)
+    },
+    scroll (pos) {
+      this.scrollY = pos.y
+    },
+    _scrollTo (index) {
+      console.log(index)
+      if (!index && index !== 0) {
+        return
+      }
+      if (index < 0) {
+        index = 0
+      }
+      this.scrollY = -this.listHeight[index]
+      this.$refs.listview.scrollToElement(this.$refs.listGroup[index])
+    },
+    _calculateHeight () {
+      this.listHeight = []
+      let height = 0
+      const list = this.$refs.listGroup
+      for (let i = 0; i < list.length; i++) {
+        this.listHeight.push(height)
+        height += list[i].clientHeight
+      }
+    }
+  },
+  watch: {
+    data () {
+      setTimeout(() => {
+        this._calculateHeight()
+      }, 20)
+    },
+    scrollY (newY) {
+      const listHeight = this.listHeight
+      for (let i = 1; i < listHeight.length; i++) {
+        if ((-newY) >= listHeight[i - 1] && (-newY) < listHeight[i]) {
+          this.currentIndex = i - 1
+          this.diff = listHeight[i] + newY
+          return
+        } else if (newY >= 0) {
+          this.currentIndex = 0
+          return
+        }
+      }
+      this.currentIndex = listHeight.length - 1
+    },
+    diff (newVal) {
+      console.log(newVal)
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+      if (this.fixedTop === fixedTop) {
+        return
+      }
+      console.log(fixedTop)
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+  .listview
+    position: relative
+    width: 100%
+    height: 100%
+    overflow: hidden
+    background: $color-background
+    .list-group
+      padding-bottom: 30px
+      .list-group-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+      .list-group-item
+        display: flex
+        align-items: center
+        padding: 20px 0 0 30px
+        .avatar
+          width: 50px
+          height: 50px
+          border-radius: 50%
+        .name
+          margin-left: 20px
+          color: $color-text-l
+          font-size: $font-size-medium
+    .list-shortcut
+      position: absolute
+      z-index: 30
+      right: 0
+      top: 50%
+      transform: translateY(-50%)
+      width: 20px
+      padding: 20px 0
+      border-radius: 10px
+      text-align: center
+      background: $color-background-d
+      font-family: Helvetica
+      .item
+        padding: 3px
+        line-height: 1
+        color: $color-text-l
+        font-size: $font-size-small
+        &.current
+          color: $color-theme
+    .list-fixed
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      .fixed-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
+</style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,90 @@
 <template>
-  <div>
-    歌手页面
+  <div class="singer">
+    <list-view :data="singerList" @select="selectSinger"></list-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {}
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+
+import ListView from 'base/listview/listview'
+
+const HOT_NAME = '热门'
+const HOT_LENGTH = 10
+
+export default {
+  components: {
+    ListView
+  },
+  data () {
+    return {
+      singerList: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    selectSinger (singer) {
+      this.$router.push({
+        path: `/singer/${singer.id}`
+      })
+    },
+    _getSingerList () {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singerList = this._normalizeSingerList(res.data.list)
+        }
+      })
+    },
+    _normalizeSingerList (list) {
+      // 先分类；再将分类后结果转为数组
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_LENGTH) {
+          map.hot.items.push(new Singer({id: item.Fsinger_mid, name: item.Fsinger_name}))
+        }
+        let key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({id: item.Fsinger_mid, name: item.Fsinger_name}))
+      })
+      // 将map转为有序列表
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  }
+}
 </script>
 
 <style lang="stylus" scoped>
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
